5915d422114ad276484b9e9efeb6644df3c2cff7
minlingz <minling.zhou@gmail.com>
2023-10-08 13:01:33 -0400

    update query

--- dynamodb.py
+++ dynamodb.py
@@ -1,19 +1,21 @@
 import boto3
-from boto3.dynamodb.conditions import Key
+import csv
+import json
+import codecs
+from itertools import groupby
+
 
 def create_table(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
     table = dynamodb.create_table(
-        TableName="courses",
-        KeySchema=[
-            {"AttributeName": "course_id", "KeyType": "HASH"}  # Partition key
-        ],
-        AttributeDefinitions=[{"AttributeName": "course_id", "AttributeType": "N"}],
+        TableName="ciu",
+        KeySchema=[{"AttributeName": "ranking", "KeyType": "HASH"}],
+        AttributeDefinitions=[{"AttributeName": "ranking", "AttributeType": "S"}],
         ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
     )
-    
+
     return table
 
 
@@ -28,55 +30,98 @@ def put_item(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
-    response = table.put_item(Item={"course_id": 706, "name": "Data Engineering"})
+    table = dynamodb.Table("ciu")
+    response = table.put_item(Item={"ranking": "0", "name": "dummy"})
 
     return response
 
 
-def get_item(dynamodb=None):
+def get_item(dynamodb=None, rank=1):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
-    response = table.get_item(Key={"course_id": 706})
-
+    table = dynamodb.Table("ciu")
+    response = table.get_item(Key={"ranking": str(rank)})
+    print(json.dumps(response["Item"], indent=4))
     return response
 
 
 def update_item(dynamodb=None):
-    """Updates the name for course_id = 706 to DE"""
-
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
+    table = dynamodb.Table("ciu")
 
     response = table.update_item(
-        Key={"course_id": 706},
+        Key={"ranking": "0"},
         UpdateExpression="SET #name = :new_name",
         ExpressionAttributeNames={"#name": "name"},
-        ExpressionAttributeValues={":new_name": "DE"},
+        ExpressionAttributeValues={":new_name": "United Nations"},
         ReturnValues="UPDATED_NEW",
     )
 
 
-def query_items(dynamodb=None):
+def query_region_user(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
+    table = dynamodb.Table("ciu")
 
-    response = table.query(KeyConditionExpression=Key("course_id").eq(706))
+    response = table.scan(
+        ProjectionExpression="#r, #v",
+        ExpressionAttributeNames={"#r": "region", "#v": "value"},
+    )
 
-    return response
+    # Sort items based on the 'region' key
+    sorted_response = sorted(response["Items"], key=lambda x: x["region"])
+
+    # Group items by the 'region' key
+    grouped_response = groupby(sorted_response, key=lambda x: x["region"])
+
+    # Dictionary to store total and average values for each region
+    region_data = {}
+
+    # Iterate through the grouped items and calculate the sum and average user value
+    for region, items in grouped_response:
+        region_sum = 0
+        count = 0
+
+        # Calculate the sum of values and count of items in the current group
+        for item in items:
+            region_sum += int(item["value"].replace(",", ""))
+            count += 1
+
+        # Calculate average user value
+        average_value = int(region_sum / count) if count != 0 else 0
+
+        # Format the total and average values with commas as thousands separators
+        formatted_sum = format(region_sum, ",d")
+        formatted_average = format(average_value, ",d")
+
+        # Store total and average values in the dictionary
+        region_data[region] = {
+            "total_users": formatted_sum,
+            "average_users_per_country": formatted_average,
+            "number_of_countries": count,
+        }
+
+    # Sort the dictionary by value in descending order
+    sorted_region_data = dict(
+        sorted(
+            region_data.items(),
+            key=lambda x: int(x[1]["total_users"].replace(",", "")),
+            reverse=True,
+        )
+    )
+    # Print the dictionary containing total and average values for each region
+    print(json.dumps(sorted_region_data, indent=4))
 
 
 def scan_items(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
+    table = dynamodb.Table("ciu")
     response = table.scan()
 
     return response
@@ -86,8 +131,8 @@ def delete_item(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
-    response = table.delete_item(Key={"course_id": 706})
+    table = dynamodb.Table("ciu")
+    response = table.delete_item(Key={"ranking": "0"})
 
     return response
 
@@ -96,7 +141,29 @@ def delete_table(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
 
-    table = dynamodb.Table("courses")
+    table = dynamodb.Table("ciu")
     table.delete()
+
+    return table
+
+
+def load_csv(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource("dynamodb")
+
+    table = dynamodb.Table("ciu")
+
+    with codecs.open("country_internet_user.csv", "r", encoding="utf-8-sig") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        for row in reader:
+            item = {
+                "name": row["name"],
+                "slug": row["slug"],
+                "value": row["value"],
+                "date_of_information": row["date_of_information"],
+                "ranking": row["ranking"],
+                "region": row["region"],
+            }
+            table.put_item(Item=item)
 
     return table
--- main.py
+++ main.py
@@ -2,7 +2,18 @@
 ETL-Query script
 """
 import fire
-from dynamodb import create_table,list_tables,put_item,get_item,update_item,query_items,scan_items,delete_item,delete_table
+from dynamodb import (
+    create_table,
+    list_tables,
+    put_item,
+    get_item,
+    update_item,
+    query_region_user,
+    scan_items,
+    delete_item,
+    delete_table,
+    load_csv,
+)
 import boto3
 
 
@@ -10,7 +21,7 @@ class QueryCLI:
     def create_table(self):
         """Extract data from source"""
         create_table()
-    
+
     def list_tables(self):
         """Extract data from source"""
         table = list_tables()
@@ -19,26 +30,23 @@ class QueryCLI:
     def put_item(self):
         """load data into destination"""
         put_item()
-    
-    def get_item(self):
+
+    def get_item(self, rank=1):
         """get data"""
-        response = get_item()
-        print(response["Item"])
-        
+        response = get_item(rank=rank)
 
     def update_item(self):
         """update data into destination"""
         update_item()
 
-    def query_items(self):
+    def query_region_user(self):
         """query data"""
-        response = query_items()
-        print(response["Items"])
+        response = query_region_user()
 
     def scan_items(self):
         """scan data"""
         scan_items()
-    
+
     def delete_item(self):
         """delete data"""
         delete_item()
@@ -46,6 +54,10 @@ class QueryCLI:
     def delete_table(self):
         """delete table"""
         delete_table()
+
+    def load_csv(self):
+        """load csv"""
+        load_csv()
 
 
 if __name__ == "__main__":
